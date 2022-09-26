616ac716bddce2e3aba48b142c9146b2f2a6a921
MatheusOliveira1996 <matheus.hortolandia@gmail.com>
2022-09-25 23:19:15 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+/* Não deixar nome do "namespace" o mesmo de "DateTime*/
+
+namespace DateTimeTeste {
+    class Program {
+        static void Main (string[] args) {
+
+            DateTime d1 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd",
+            CultureInfo.InvariantCulture);
+
+            DateTime d2 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss",
+            CultureInfo.InvariantCulture);
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+
+        }
+    }
+}
